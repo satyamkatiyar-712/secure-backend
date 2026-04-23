3ded5888fe4e618a0bd9d86b570fe1abda832388
satyam <katiyarsatyam712@gmail.com>
2026-04-23 23:33:28 +0530

    feat: implemented update note API with ownership security check

--- controller/notes.controller.js
+++ controller/notes.controller.js
@@ -24,3 +24,32 @@ export const CreateNote=CatchError(async(req,res)=>{
 
 })
 
+export const UpdateNote=CatchError(async(req,res)=>{
+
+         const noteId= req.params.id
+             const {title,content}=req.body
+            if(!title || !content){
+                throw new AppError(400,"Provide the detail to update the note")
+            }
+
+            const note = await NOTE.findById(noteId)
+            if(!note){
+                throw new AppError(404,"No note found")
+            }
+
+            //security check
+            if(note.owner.toString()!==req.user.userId){
+                throw new AppError(403,"Unauthorized not access to note")
+            }
+
+            const updatedNote= await NOTE.findByIdAndUpdate(
+                noteId,
+                {title,content},
+                { new: true, runValidators: true }
+            )
+           res.status(200).json({
+               success:true,
+               message:"Note updated successfully",
+               note:updatedNote
+           })
+})
--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import {CreateNote} from '../controller/notes.controller.js'
+import {CreateNote,UpdateNote} from '../controller/notes.controller.js'
 import {authHandlerMiddleware} from '../Middleware/authHandlerMiddleware.js'
 
 const router=Router()
 
 router.post("/create",authHandlerMiddleware,CreateNote)
+router.put("/update/:id",authHandlerMiddleware,UpdateNote)
 
 
 export default router
