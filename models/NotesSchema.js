5075562000fcac033361ad6fb5ac2a37f99b0e16
satyam <katiyarsatyam712@gmail.com>
2026-04-23 18:02:37 +0530

    feat: created Notes model with user reference and validation

---
+++ models/NotesSchema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const NotesSchema= mongoose.Schema({
+      title:{
+         type:String,
+         required:true,
+         trim:true
+      },
+      content:{
+         type:String,
+         required:true
+      },
+      owner:{
+         type:mongoose.Schema.Types.ObjectId,
+         required:true,
+         ref:'USER'
+      },
+      Tags:{
+        type:[String],
+        enum:["Work","Imporant","Idea","Creative"],
+        default:''
+      },
+      isPinned:{
+        type:Boolean,
+        default:false
+      }
+},{timestamps:true})
+
+export const NOTE=mongoose.model("usernotes",NotesSchema)
